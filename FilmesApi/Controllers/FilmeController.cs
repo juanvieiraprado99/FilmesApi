9ef0a2a60f3471a15c79433f61e5332c72038eb8
juanprado <juan.prado@cacaushow.com.br>
2023-10-20 18:04:37 -0300

    feat: add busca por id e paginação no retorno

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,12 +8,26 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody]Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "O titulo do filme é obrigatório!")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O gênero do filme é obrigatório!")]
@@ -12,5 +13,6 @@ namespace FilmesApi.Models
         [Required(ErrorMessage = "O duração do filme é obrigatória!")]
         [Range(70, 600, ErrorMessage = "A duração deve ser de 70 a 600 minutos.")]
         public int Duracao { get; set; }
+        
     }
 }
