787af547b596f18bbb314849ba6e386ef7c0c592
juanprado <juan.prado@cacaushow.com.br>
2023-10-26 07:34:07 -0300

    feat: Ajuste para realizar ações pelo banco

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -7,15 +8,19 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new List<Filme>();
-    private static int id = 0;
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult AdicionaFilme([FromBody]Filme filme)
     {
         // Requisição com body deve ser passado no corpo da requisição
-        filme.Id = id++;
-        filmes.Add(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
         // Retorna o item que foi criado
         return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
     }
@@ -24,13 +29,13 @@ public class FilmeController : ControllerBase
     public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, int take = 10)
     {
         // Requisição com query deve ser passado como parametros na rota
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
         if (filme == null) return NotFound();
         return Ok(filme);
