e774630f52a400550792c121b4892ce85ea2cf19
juanprado <juan.prado@cacaushow.com.br>
2023-10-26 07:26:19 -0300

    feat: conexão com o banco de gerando migration

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data
+{
+    public class FilmeContext : DbContext
+    {
+        // Adicionar uma migration, Add-Migration nomeDaMigration
+
+        // Atualizar o banco, Update-Database
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+            : base(opts)
+        {
+            
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
