ba6ef3d7aa20dfb57c6dfc11eb50209ae8486241
juanprado <juan.prado@cacaushow.com.br>
2023-10-30 12:35:18 -0300

    feat: documentação a mais para o swagger

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -15,6 +15,5 @@ namespace FilmesApi.Models
         [Required(ErrorMessage = "O duração do filme é obrigatória!")]
         [Range(70, 600, ErrorMessage = "A duração deve ser de 70 a 600 minutos.")]
         public int Duracao { get; set; }
-        
     }
 }
